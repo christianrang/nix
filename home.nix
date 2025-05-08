2d5c4e84a8e69c711740fcef1fff63732d8a0f7d
Christian Rang <cj@rang-corp.com>
2025-05-08 10:58:26 -0400

    chore: little cleanup

--- home.nix
+++ home.nix
@@ -58,8 +58,6 @@ in {
     pkgs.picom
   ]);
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
   home.file = {
     ".zshrc".source = zsh/.zshrc;
     ".i3/config".source = i3/.i3/config;
@@ -97,27 +95,10 @@ in {
     };
   };
 
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/crang/etc/profile.d/hm-session-vars.sh
-  #
   home.sessionVariables = {
     # EDITOR = "emacs";
   };
 
-  # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
   programs.zsh = {
     enable = true;
