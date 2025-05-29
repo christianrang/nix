f82221f679e0bb041b15cb1e464f92f0ead8cda6
Christian Rang <cj@rang-corp.com>
2025-05-29 08:19:20 -0400

    chore(nvim): add keymap

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -12,3 +12,5 @@ vim.keymap.set("n", "<leader>j", ":cnext<CR>", { desc =  "Next in Quickfix" })
 vim.keymap.set("n", "<leader>k", ":cprev<CR>", { desc =  "Previous in Quickfix" })
 
 vim.keymap.set("n", "<leader>cd", ":cd %:p:h<CR>:pwd<CR>", { desc = "Change working directory to the directory of open file" })
+
+vim.keymap.set("n", "<leader>z", ":tab split<CR>", { desc = "Fullscreen current window" })
