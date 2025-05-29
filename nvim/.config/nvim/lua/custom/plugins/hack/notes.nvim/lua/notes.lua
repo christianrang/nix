12c5e8cb4fd8b758ba593cab7bd71f343a596ffd
Christian Rang <christian.rang@fishtech.group>
2025-05-29 08:26:00 -0400

    chore(nvim): formatting notes.lua

--- nvim/.config/nvim/lua/custom/plugins/hack/notes.nvim/lua/notes.lua
+++ nvim/.config/nvim/lua/custom/plugins/hack/notes.nvim/lua/notes.lua
@@ -7,110 +7,109 @@ local defaults = {}
 
 --- @type notes.Options
 local options = {
-    base_path = os.getenv("HOME") .. "/vaults/vault/daily",
+  base_path = os.getenv 'HOME' .. '/vaults/vault/daily',
 }
 
 --- @param opts notes.Options
 M.setup = function(opts)
-    options = vim.tbl_deep_extend("force", defaults, opts or {})
+  options = vim.tbl_deep_extend('force', defaults, opts or {})
 end
 
 local function get_daily_path()
-    local full_filepath = options.base_path .. os.date("/%Y/%m/%d/")
+  local full_filepath = options.base_path .. os.date '/%Y/%m/%d/'
 
-    return full_filepath
+  return full_filepath
 end
 
 M.daily_note = function()
-    local filename = "daily_" .. os.date("%Y-%m-%d") .. ".md"
-    local filepath = get_daily_path()
+  local filename = 'daily_' .. os.date '%Y-%m-%d' .. '.md'
+  local filepath = get_daily_path()
 
-    local function is_buffer_exists(options)
-        local buffer_name = options.buffer_name
+  local function is_buffer_exists(options)
+    local buffer_name = options.buffer_name
 
-        for buffer = 1, vim.fn.bufnr('$') do
-            if buffer_name == vim.fn.bufname(buffer) then
-                return true
-            end
-        end
-
-        return false
+    for buffer = 1, vim.fn.bufnr '$' do
+      if buffer_name == vim.fn.bufname(buffer) then
+        return true
+      end
     end
 
-    if vim.fn.isdirectory(filepath) == 0 then
-        os.execute("mkdir -p " .. filepath)
-    end
+    return false
+  end
 
-    local function file_exists(name)
-        local f=io.open(name,"r")
-        if f~=nil then
-            io.close(f)
-            return true
-        else
-            return false
-        end
-    end
+  if vim.fn.isdirectory(filepath) == 0 then
+    os.execute('mkdir -p ' .. filepath)
+  end
 
-    if file_exists(filepath .. filename) == false then
-        print("file does not exist: creating: " .. filepath .. filename)
-        os.execute("touch " .. filepath .. filename)
-        -- os.execute("tmpl-daily -o " .. filepath .. filename)
-    end
-
-    local exists = is_buffer_exists({buffer_name = filepath .. filename})
-
-    if exists then
-        vim.cmd("bw " .. filepath .. filename)
+  local function file_exists(name)
+    local f = io.open(name, 'r')
+    if f ~= nil then
+      io.close(f)
+      return true
     else
-        vim.cmd("e " .. filepath .. filename)
+      return false
     end
+  end
+
+  if file_exists(filepath .. filename) == false then
+    print('file does not exist: creating: ' .. filepath .. filename)
+    os.execute('touch ' .. filepath .. filename)
+    -- os.execute("tmpl-daily -o " .. filepath .. filename)
+  end
+
+  local exists = is_buffer_exists { buffer_name = filepath .. filename }
+
+  if exists then
+    vim.cmd('bw ' .. filepath .. filename)
+  else
+    vim.cmd('e ' .. filepath .. filename)
+  end
 end
 
-
 M.tomorrow_daily_note = function()
-    local filename = "daily_" .. os.date("%Y-%m-%d",os.time()+24*60*60) .. ".md"
-    local filepath = get_daily_path()
-    print(filepath)
+  local filename = 'daily_' .. os.date('%Y-%m-%d', os.time() + 24 * 60 * 60) .. '.md'
+  local filepath = get_daily_path()
+  print(filepath)
 
-    local function is_buffer_exists(options)
-        local buffer_name = options.buffer_name
+  local function is_buffer_exists(options)
+    local buffer_name = options.buffer_name
 
-        for buffer = 1, vim.fn.bufnr('$') do
-            if buffer_name == vim.fn.bufname(buffer) then
-                return true
-            end
-        end
-
-        return false
+    for buffer = 1, vim.fn.bufnr '$' do
+      if buffer_name == vim.fn.bufname(buffer) then
+        return true
+      end
     end
 
-    if vim.fn.isdirectory(filepath) == 0 then
-        os.execute("mkdir -p " .. filepath)
-    end
+    return false
+  end
 
-    local function file_exists(name)
-        local f=io.open(name,"r")
-        if f~=nil then
-            io.close(f)
-            return true
-        else
-            return false
-        end
-    end
+  if vim.fn.isdirectory(filepath) == 0 then
+    os.execute('mkdir -p ' .. filepath)
+  end
 
-    if file_exists(filepath .. filename) == false then
-        print("file does not exist: creating: " .. filepath .. filename)
-        os.execute("touch " .. filepath .. filename)
-        -- os.execute("tmpl-daily -o " .. filepath .. filename)
-    end
-
-    local exists = is_buffer_exists({buffer_name = filepath .. filename})
-
-    if exists then
-        vim.cmd("bw " .. filepath .. filename)
+  local function file_exists(name)
+    local f = io.open(name, 'r')
+    if f ~= nil then
+      io.close(f)
+      return true
     else
-        vim.cmd("e " .. filepath .. filename)
+      return false
     end
+  end
+
+  if file_exists(filepath .. filename) == false then
+    print('file does not exist: creating: ' .. filepath .. filename)
+    os.execute('touch ' .. filepath .. filename)
+    -- os.execute("tmpl-daily -o " .. filepath .. filename)
+  end
+
+  local exists = is_buffer_exists { buffer_name = filepath .. filename }
+
+  if exists then
+    vim.cmd('bw ' .. filepath .. filename)
+  else
+    vim.cmd('e ' .. filepath .. filename)
+  end
 end
 
 return M
