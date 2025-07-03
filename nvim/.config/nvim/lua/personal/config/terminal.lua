fd5f7c6bc52d2e6d045948bd5b3c7258256402ae
Christian Rang <cj@rang-corp.com>
2025-07-03 12:31:08 -0400

    chore(nvim): format terminal.lua

--- nvim/.config/nvim/lua/personal/config/terminal.lua
+++ nvim/.config/nvim/lua/personal/config/terminal.lua
@@ -1,6 +1,6 @@
 vim.api.nvim_create_autocmd('TermOpen', {
     group = vim.api.nvim_create_augroup('custom-term-open', { clear = true }),
-    callback = function ()
+    callback = function()
         vim.opt.scrolloff = 0
     end,
 })
@@ -14,17 +14,17 @@ vim.keymap.set("n", "<leader>st", function()
 end)
 
 local strsplit = function(inputstr, sep)
-  if sep == nil then
-    sep = "%s"
-  end
-  local t = {}
-  for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
-    table.insert(t, str)
-  end
-  return t
+    if sep == nil then
+        sep = "%s"
+    end
+    local t = {}
+    for str in string.gmatch(inputstr, "([^" .. sep .. "]+)") do
+        table.insert(t, str)
+    end
+    return t
 end
 
-local decide_command = function ()
+local decide_command = function()
     local pathTable = strsplit(vim.api.nvim_buf_get_name(0), "/")
 
     if pathTable[#pathTable] == "home.nix" then
@@ -34,8 +34,8 @@ local decide_command = function ()
     local filetype_to_command = {
         sh = vim.api.nvim_buf_get_name(0),
         bash = vim.api.nvim_buf_get_name(0),
-        python = 'python3 '..vim.api.nvim_buf_get_name(0),
-        go =  'go run '..vim.api.nvim_buf_get_name(0)
+        python = 'python3 ' .. vim.api.nvim_buf_get_name(0),
+        go = 'go run ' .. vim.api.nvim_buf_get_name(0)
     }
 
     local command = filetype_to_command[vim.bo.filetype]
@@ -70,7 +70,7 @@ vim.keymap.set("n", "<leader>rc", function()
         job_id = vim.bo.channel
     end
 
-    vim.fn.chansend(job_id, { command.."\r\n" })
+    vim.fn.chansend(job_id, { command .. "\r\n" })
 
     -- vim.fn.chansend(job_id, { "go run cmd/*/main.go\r\n" })
 end, { desc = "[R]un [c]code" })
