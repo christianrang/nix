5fce18abf2f757ac0e9305992189ebee254c871e
Christian Rang <cj@rang-corp.com>
2026-04-22 16:29:36 -0400

    chore(nix): updating flake lock and nix

--- flake.nix
+++ flake.nix
@@ -14,16 +14,20 @@
     };
   };
 
-  outputs = { nixpkgs, home-manager, ... }@inputs: let
-    mkHome = system: module: home-manager.lib.homeManagerConfiguration {
-      pkgs = nixpkgs.legacyPackages.${system};
-      modules = [ module ];
-      extraSpecialArgs = { inherit inputs; };
+  outputs = { nixpkgs, home-manager, ... }@inputs:
+    let
+      mkHome = system: module:
+        home-manager.lib.homeManagerConfiguration {
+          pkgs = nixpkgs.legacyPackages.${system};
+          modules = [ module ];
+          extraSpecialArgs = { inherit inputs; };
+        };
+    in {
+      homeConfigurations = {
+        "christian.rang" =
+          mkHome "aarch64-darwin" ./nix/hosts/runreveal-macbook/home.nix;
+        "root" = mkHome "x86_64-linux" ./nix/hosts/dev/home.nix;
+        "crang" = mkHome "x86_64-linux" ./nix/hosts/framework/home.nix;
+      };
     };
-  in {
-    homeConfigurations = {
-      "christian.rang" = mkHome "aarch64-darwin" ./nix/hosts/runreveal-macbook/home.nix;
-      "root"           = mkHome "x86_64-linux"   ./nix/hosts/dev/home.nix;
-    };
-  };
 }
