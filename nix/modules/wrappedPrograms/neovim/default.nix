cdc519d77110486435afcb120885d6a9a2f01a6b
Christian Rang <cj@rang-corp.com>
2026-06-05 14:02:59 -0400

    feat(neovim): get neovim working on dendritic

--- nix/modules/wrappedPrograms/neovim/default.nix
+++ nix/modules/wrappedPrograms/neovim/default.nix
@@ -1,4 +1,16 @@
 { inputs, self, ... }: {
-  flake.modules.neovim = { pkgs, ... }: { programs.neovim.enable = true; };
+  flake.modules.neovim = { pkgs, ... }: {
+    programs.neovim.enable = true;
+
+    home.file = {
+      ".config/nvim/" = {
+        enable = true;
+        source = ./.config/nvim;
+        recursive = true;
+      };
+    };
+
+    home.sessionVariables = { EDITOR = "nvim"; };
+  };
   perSystem = { pkgs, self', ... }: { };
 }
