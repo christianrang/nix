befc9c1a35bcfeb9861e6cc9011aaf2249d17fd0
Christian Rang <cj@rang-corp.com>
2026-06-05 14:30:55 -0400

    update neovim nix file

--- nix/modules/wrappedPrograms/neovim/default.nix
+++ nix/modules/wrappedPrograms/neovim/default.nix
@@ -16,7 +16,7 @@
     apps.neovim = {
       type = "app";
       program = "${pkgs.writeShellScript "neovim" ''
-        ln -sf ${../../../nvim/.config/nvim} "$HOME/.config/nvim"
+        ln -sf ${./.config/nvim} "$HOME/.config/nvim"
         exec ${pkgs.lib.getExe pkgs.neovim} "$@"
       ''}";
     };
