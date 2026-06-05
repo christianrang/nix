9f1b29e9f264fa521eb1634654bc225c148e45e4
Christian Rang <cj@rang-corp.com>
2026-06-05 14:13:26 -0400

    feat(alacritty): get alacritty working on dendritic

--- nix/modules/wrappedPrograms/alacritty/default.nix
+++ nix/modules/wrappedPrograms/alacritty/default.nix
@@ -1,6 +1,15 @@
 { inputs, self, ... }: {
   flake.modules.alacritty = { pkgs, ... }: {
-    programs.alacritty.enable = true;
+    home.packages = with pkgs;
+      [ alacritty nerd-fonts.hack ]
+      ++ (lib.optionals (pkgs.stdenv.isLinux) [ ghostty ]);
+
+    home.file = {
+      ".config/alacritty/" = {
+        source = ./.config/alacritty;
+        recursive = true;
+      };
+    };
   };
   perSystem = { pkgs, self', ... }: { };
 }
